433a2112553aab4eafd6d242259983e32de5da58
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-20 19:06:59 +0300

    Feat(NPM) Added emotion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import { Container } from './App.styled';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Container>Ku Ku</Container>;
 };
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 800px;
+  padding: 100px;
+  margin: 0 auto;
+  background-color: blueviolet;
+`;
